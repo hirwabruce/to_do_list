f64e4e225d0ee66a0f6104501997771ad0ffd401
Bruce Hirwa <hirwabruce00@gmail.com>
2026-07-02 14:28:16 +0200

    Changes in main

--- main.py
+++ main.py
@@ -22,15 +22,14 @@ print("2. Profile")
 print("3. Exit")
 choice_1=input("Enter your choice(1-3): ")
 if choice_1=='2':
-    view_person_info(person_info)
-    """
+    
     print("Welcome to your profile")
     print("1.Edit the profile")
     print("2.View the profie")
     choice_2=input("Type'edit'or'1' to edit the profile and 'view'or '2' to view the profile. ")
     if choice_2.lower=='2' or choice_2.lower=='view':
-        view_person_info
-    """
+        view_person_info(person_info)
+    
 #add_task()
 #save_tasks()
 #view_tasks()
